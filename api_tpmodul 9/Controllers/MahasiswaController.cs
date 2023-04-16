e71267f1fb0ab7b5cd2d785893c91484c63bd8d1
hermwnS <126874286+hermwnS@users.noreply.github.com>
2023-04-16 15:49:53 +0700

    menambahkan controller dan class Mahasiswa

---
+++ api_tpmodul 9/Controllers/MahasiswaController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace api_tpmodul_9.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> mahasiswaData = new List<Mahasiswa>
+        {
+            new Mahasiswa("Hermawan Saputra", "1302213080"),
+            new Mahasiswa("Triani Putri Mumpuni", "130221"),
+            new Mahasiswa("Dhafa Arizki Gusman", "130221"),
+            new Mahasiswa("Arzaq Ajradika", "130221"),
+            new Mahasiswa("Yesa Kristanto", "130221")
+        };
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return mahasiswaData;
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return mahasiswaData[id];
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa value)
+        {
+            mahasiswaData.Add(value);
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Mahasiswa value)
+        {
+            mahasiswaData[id] = value;
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            mahasiswaData.RemoveAt(id);
+        }
+    }
+}
---
+++ api_tpmodul 9/Mahasiswa.cs
@@ -0,0 +1,14 @@
+﻿namespace api_tpmodul_9
+{
+    public class Mahasiswa
+    {
+        private string Nama { get; set; }
+        private string Nim { get; set; }
+
+        public Mahasiswa(string Nama, string Nim)
+        {
+            this.Nama = Nama;
+            this.Nim = Nim;
+        }
+    }
+}
